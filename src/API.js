30456e32749eee805e02b53df3981a94a8a6fbb1
Disha Mahna <dishamahna1612@gmail.com>
2021-01-08 15:39:20 +1300

    API URL changed from ngrok to AWS

--- src/API.js
+++ src/API.js
@@ -1,1 +1,2 @@
-export const API_LINK = "https://5a7b971aac43.ngrok.io";
+export const API_LINK =
+  "https://cors-anywhere.herokuapp.com/http://socialnotes-env.eba-r5vr2pzm.us-east-2.elasticbeanstalk.com";
